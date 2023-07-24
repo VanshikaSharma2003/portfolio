b74edd0f9a81d7e460984fee9c080578f89a960e
VanshikaSharma2003 <139851911+VanshikaSharma2003@users.noreply.github.com>
2023-07-24 10:57:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-end Web Developer",  "Freelancer"],
+        strings: ["Front-end Web Developer",  "UI/UX Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front-end Web Developer",  "Freelancer"],
+        strings: ["Front-end Web Developer", "UI/UX Developer"  ,"Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
